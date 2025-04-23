8ed838f337bf87c78b91b8bd9e2abbcef10428c3
umutbarandemir <baran292907@hotmail.com>
2025-04-23 16:24:03 +0300

    text model created

---
+++ Backend/src/models/textModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const textSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    text: {
+        type: String,
+    },
+    image:{
+        type: String,
+    }
+},{timestamps:true});
+
+const Text = mongoose.model("Text", textSchema); // Create a model named "Text" based on the textSchema
+
+export default Text; // Export the Text model based on the textSchema
