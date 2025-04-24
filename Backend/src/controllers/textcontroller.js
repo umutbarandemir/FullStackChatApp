ae008f01397fe1966b9d5a455f7cf5dd48d9610c
umutbarandemir <baran292907@hotmail.com>
2025-04-24 22:07:47 +0300

    getText and sendText routes and functions added

--- Backend/src/controllers/textcontroller.js
+++ Backend/src/controllers/textcontroller.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js"; // Import the User model
+import Text from "../models/textModel.js"; // Import the Text model
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -10,5 +11,58 @@ export const getUsersForSidebar = async (req, res) => {
     } catch (error) {
         console.error("Error fetching users for sidebar:", error); // Log the error to the console
         res.status(500).json({ message: "Internal server error" }); // Send a 500 response if an error occurs
+    }
+}
+
+export const getTexts = async (req, res) => {
+    try {
+        const { id } = req.params; // Get the ID from the request parameters
+        const loggedInUserId = req.user._id; // Get the logged-in user's ID from the request object
+
+        const texts = await Text.find({
+            $or: [
+                { sender: loggedInUserId, receiver: id }, // Find texts where the logged-in user is the sender and the other user is the receiver
+                { sender: id, receiver: loggedInUserId } // Find texts where the other user is the sender and the logged-in user is the receiver
+            ]
+        });
+
+        res.status(200).json(texts); // Send the texts as a response with a 200 status code
+
+    } catch (error) {
+        console.error("Error fetching texts:", error); // Log the error to the console
+        res.status(500).json({ message: "Internal server error" }); // Send a 500 response if an error occurs
+    }
+}
+
+export const sendTexts = async (req, res) => {
+    try {
+        const { text , image } = req.body; // Get the text and image from the request body
+        const { id:receiverId } = req.params; // Get the ID from the request parameters
+        const senderID = req.user._id; // Get the logged-in user's ID from the request object
+
+        let imageUrl = null; // Initialize imageUrl to null
+
+        if(image) { // If an image is provided
+            const uploadResponse = await cloudinary.uploader.upload(image) // Upload the image to Cloudinary
+            imageUrl = uploadResponse.secure_url; // Get the secure URL of the uploaded image
+        }
+
+        let newText = await Text.create({ // Create a new text message
+            text,
+            image: imageUrl,
+            senderId: senderID,
+            receiverId: receiverId
+        });
+
+        await newText.save(); // Save the new text message to the database
+
+        //this is where the functionality of sending the message to the receiver will be implemented with socket.io
+
+        res.status(201).json(newText); // Send the new text message as a response with a 200 status code
+
+    } catch (error) {
+        console.error("Error sending texts:", error); // Log the error to the console
+        res.status(500).json({ message: "Internal server error" }); // Send a 500 response if an error occurs
+        
     }
 }
--- Backend/src/routes/text.js
+++ Backend/src/routes/text.js
@@ -1,9 +1,13 @@
 import express from "express"; // Importing express to create a router
 import { protectRoute } from "../middleware/authMiddleware.js";
-import { getUsersForSidebar } from "../controllers/textcontroller.js"; // Importing the getUsersForSidebar controller
+import { getUsersForSidebar, getTexts, sendTexts } from "../controllers/textcontroller.js"; // Importing the getUsersForSidebar controller
 
 const router = express.Router(); 
 
 router.get("/users",protectRoute, getUsersForSidebar); // Define a GET route for users using the getUsersForSidebar controller
+
+router.get("/:id", protectRoute, getTexts); // Define a GET route for texts using the getTexts controller
+
+router.post("/send/:id", protectRoute, sendTexts); // Define a GET route for sending texts using the getTexts controller
 
 export default router; // Export the router
