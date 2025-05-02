0ac9bef2f36fa9110e669af9b924e6dcacfe6397
umutbarandemir <baran292907@hotmail.com>
2025-05-03 02:12:37 +0300

    text skeleton for loading, chat header created

--- Frontend/src/components/ChatContainer.jsx
+++ Frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore.js'
+import { useAuthStore } from '../store/useAuthStore.js'
+import TextInput from './TextInput.jsx'
+import ChatHeader from './ChatHeader.jsx'
+import TextSkeleton from './skeletons/TextSkeleton.jsx'
+
 
 function ChatContainer() {
+
+  const { getTexts, isTextsLoading, selectedUser} = useChatStore();
+
+  // const { authUser } = useAuthStore();
+
+  useEffect(()=>{
+    getTexts(selectedUser._id);
+  },[selectedUser._id, getTexts]);
+
+  if (isTextsLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <TextSkeleton />
+        <TextInput />
+      </div>
+    );
+  }
+
   return (
-    <div>ChatContainer</div>
+    <div className="flex-1 flex flex-col overflow-auto">
+    <ChatHeader />
+    <div>messagesssss</div>
+    <TextInput />
+  </div>
   )
 }
 
